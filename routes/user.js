124ed2c3eb5e3f0887e9f3707817d7704a27020e
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-04-23 15:18:51 +0100

    Change filepath names.
    Co-authored-by: Giovanna <to.gio@pm.me>
    Co-authored-by: Alexreid95 <alexreid95@gmail.com>
    Co-authored-by: @akomiqaia <akaki.mikaia.1@iliauni.edu.ge>

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,11 @@
 import query from "../query.js";
 const app = document.querySelector("#app");
 
-const html = html`
+const html = `
     <h1>My Doggy Page</h1>
     <section></section>
 `;
 
+function user() {}
 
-function users() {}
-
-export default users;
+export default user;
