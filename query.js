f466605d561ca0793ebdc929400231fa4192aeb1
AlexReid95 <alexreid95@gmail.com>
2020-04-23 18:20:34 +0100

    Create new-dog route Relates #14 Reltaes #15
    Co-authored-by: HettieM <hettie.mcconnell@outlook.com>

--- query.js
+++ query.js
@@ -1,16 +1,16 @@
 function query(url, options) {
     return fetch(url, options).then(res => {
-        if (!res.ok) {
-            const error = new Error("HTTP Error");
-            error.status = res.status;
-            throw error;
-        }
+        // if (!res.ok) {
+        //     const error = new Error("HTTP Error");
+        //     error.status = res.status;
+        //     throw error;
+        // }
         const contentType = res.headers.get("content-type");
         if (contentType && contentType.includes("json")) {
             return res.json();
         } else {
             return res.text();
-        } 
+        }
     });
 }
 
--- routes/new-dog.js
+++ routes/new-dog.js
@@ -10,6 +10,9 @@ const loggedInHtml = `
     <label for="breed">Dog Breed: </label>
     <input type="text" id="breed" name="breed" />
 
+    <label for="img">Dog Image URL: </label>
+    <input type="text" id="img" name="img" />
+
     <div id="message"><div>
 
     <button>Save Doggie </button>
@@ -22,24 +25,31 @@ const loggedOutHtml = `
 <a href="/sign-up">sign up</a> to continue
 </h1>`;
 
-function newDog() {
+function newDog({redirect}) {
     const token = localStorage.getItem("token");
 
     if (!token) {
         app.innerHTML = loggedOutHtml;
     } else {
         app.innerHTML = loggedInHtml;
-        app.querySelector("newDogForm").addEventListener("submit", event => {
+        app.querySelector("#newDogForm").addEventListener("submit", event => {
             event.preventDefault();
             const form = new FormData(event.target);
             const formObj = Object.fromEntries(form);
+            const ownerId = window.localStorage.getItem("userId");
+            formObj.owner = ownerId;
 
             query("https://dogs-rest.herokuapp.com/v1/dogs", {
                 method: "POST",
                 body: JSON.stringify(formObj),
                 headers: {
-                    "content-type": "application/json"
+                    "content-type": "application/json",
+                    authorization: `Bearer ${token}`
                 }
+            }).then(dogObj => {
+                console.log(dogObj);
+                // const dogId = dogObj.id;
+                // redirect(`/dog?id=${dogId}`);
             });
         });
     }
