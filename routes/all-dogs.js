73fe5451a0af07c76560860c3fdeca5c5d384dee
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-04-24 11:19:53 +0100

    Add routing for add dog functionality.
    Closes #45

--- routes/all-dogs.js
+++ routes/all-dogs.js
@@ -1,21 +1,21 @@
 import query from "../query.js";
 const app = document.querySelector("#app");
 
-// The secret code. 
-let audio = document.createElement("audio");
-audio.src = "../surprise/who-let-the-dogs-out-ringtone.mp3";
-
+// The secret code.
+// let audio = document.createElement("audio");
+// audio.src = "../surprise/who-let-the-dogs-out-ringtone.mp3";
 
 const html = /*html*/ `
 <h1>Our Fury Friends!</h1>
 <button id="mute">Stop This Madness</button>
+<a id="addDog">Add your own dog here</a>
 <ul id="allDogs" class="all-dogs-container"></ul>
 `;
 
 function createDogElement(dog) {
     const dogListItem = document.createElement("li");
-    const dogAnchor = document.createElement("a")
-    dogAnchor.href = `/dog?id=${dog.id}`
+    const dogAnchor = document.createElement("a");
+    dogAnchor.href = `/dog?id=${dog.id}`;
     const dogNameElement = document.createElement("h2");
     dogNameElement.textContent = dog.name;
     const dogBreedElement = document.createElement("h3");
@@ -35,17 +35,24 @@ function createDogElement(dog) {
 function allDogs() {
     query("https://dogs-rest.herokuapp.com/v1/dogs")
         .then(arrayDogs => {
-            audio.play()
+            // audio.play();
             app.innerHTML = html;
-            document.querySelector("#mute").addEventListener("click", () => {   
+            const addDog = document.querySelector("#addDog");
+            const token = localStorage.getItem("token");
+            if (token) {
+                addDog.href = "/new-dog";
+            } else {
+                addDog.href = "/log-in";
+            }
+            document.querySelector("#mute").addEventListener("click", () => {
                 audio.pause();
-            }); 
+            });
             arrayDogs.map(dog => createDogElement(dog));
         })
         .catch(err => console.error(err));
-    }
-    
-   // setTimeout(()=> {audio.pause(); audio.currentTime =0;}, 3000)
+}
+
+// setTimeout(()=> {audio.pause(); audio.currentTime =0;}, 3000)
 export default allDogs;
 
 // audio.play()
